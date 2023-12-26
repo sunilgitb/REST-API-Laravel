acc0db5475d5b48d451b94f19582c6575869b485
sunilgitb <84218306+sunilgitb@users.noreply.github.com>
2023-12-26 18:05:23 +0530

    Update VideoController.php

--- app/Http/Controllers/API/VideoController.php
+++ app/Http/Controllers/API/VideoController.php
@@ -1,28 +1,32 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreVideoRequest;
 use App\Jobs\ConvertVideoForDownloading;
 use App\Jobs\ConvertVideoForStreaming;
 use App\Video;
+use Illuminate\Http\JsonResponse;
 
 class VideoController extends Controller
 {
-    public function store(StoreVideoRequest $request)
+    public function store(StoreVideoRequest $request): JsonResponse
     {
-        $video = Video::create([
-            'disk'          => 'videos_disk',
-            'original_name' => $request->video->getClientOriginalName(),
-            'path'          => $request->video->store('videos', 'videos_disk'),
-            'title'         => $request->title,
-        ]);
+        try {
+            $video = Video::create([
+                'disk'          => 'videos_disk',
+                'original_name' => $request->video->getClientOriginalName(),
+                'path'          => $request->video->store('videos', 'videos_disk'),
+                'title'         => $request->title,
+            ]);
 
-        $this->dispatch(new ConvertVideoForDownloading($video));
-        $this->dispatch(new ConvertVideoForStreaming($video));
+            $this->dispatch(new ConvertVideoForDownloading($video));
+            $this->dispatch(new ConvertVideoForStreaming($video));
 
-        return response()->json([
-            'id' => $video->id,
-        ], 201);
+            return response()->json([
+                'id' => $video->id,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to store video'], 500);
+        }
     }
 }
