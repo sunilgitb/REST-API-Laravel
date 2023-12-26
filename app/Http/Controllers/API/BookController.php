84c5cad61522760354e245495d7dc1049eeed722
sunilgitb <84218306+sunilgitb@users.noreply.github.com>
2023-12-26 17:41:57 +0530

    Update BookController.php

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -1,16 +1,64 @@
 <?php
+namespace App\Http\Controllers;
+
 use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Database\QueryException;
 
 class BookController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
-        return Book::all();
+        try {
+            $books = Book::all();
+            return response()->json($books, 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to fetch books'], 500);
+        }
     }
 
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        $book = Book::create($request->all());
-        return response()->json($book, 201);
+        try {
+            $validatedData = $request->validate([
+                'title' => 'required|string',
+                'author' => 'required|string',
+                // Add validation rules for other fields if needed
+            ]);
+
+            $book = Book::create($validatedData);
+            return response()->json($book, 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to store book'], 500);
+        }
+    }
+
+    public function update(Request $request, Book $book): JsonResponse
+    {
+        try {
+            $validatedData = $request->validate([
+                'title' => 'required|string',
+                'author' => 'required|string',
+                // Add validation rules for other fields if needed
+            ]);
+
+            $book->update($validatedData);
+
+            return response()->json($book, 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to update book'], 500);
+        }
+    }
+
+    public function destroy(Book $book): JsonResponse
+    {
+        try {
+            $book->delete();
+
+            return response()->json(null, 204);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to delete book'], 500);
+        }
     }
 }
