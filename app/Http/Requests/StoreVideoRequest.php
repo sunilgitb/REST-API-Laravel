89dff171d0381f5baf154d04383f92cbca27a706
sunilgitb <84218306+sunilgitb@users.noreply.github.com>
2023-12-26 18:41:10 +0530

    Update StoreVideoRequest.php

--- app/Http/Requests/StoreVideoRequest.php
+++ app/Http/Requests/StoreVideoRequest.php
@@ -1,9 +1,31 @@
 <?php
 
-public function rules()
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VideoUploadRequest extends FormRequest
 {
-    return [
-        'title' => 'required',
-        'video' => 'required|file|mimetypes:video/mp4,video/mpeg,video/x-matroska',
-    ];
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true; // Set to true for simplicity, you might implement your own logic here
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'video' => 'required|file|mimetypes:video/mp4,video/mpeg,video/x-matroska',
+        ];
+    }
 }
