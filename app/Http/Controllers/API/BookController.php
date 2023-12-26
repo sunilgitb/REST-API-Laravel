655c6398bee0379b1704f84d7c9094dfb002c4ba
sunilgitb <84218306+sunilgitb@users.noreply.github.com>
2023-12-26 17:35:33 +0530

    Update API

---
+++ app/Http/Controllers/API/BookController.php
@@ -0,0 +1,15 @@
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        return Book::all();
+    }
+
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+        return response()->json($book, 201);
+    }
+}
