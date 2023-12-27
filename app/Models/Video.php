76271baccd5c011db4567e995f38cd133d29a543
sunilgitb <84218306+sunilgitb@users.noreply.github.com>
2023-12-27 12:25:00 +0530

    Update Video.php

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -1,15 +1,43 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
-    protected $dates = [
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'videos';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'original_name',
+        'disk',
+        'path',
         'converted_for_downloading_at',
         'converted_for_streaming_at',
     ];
 
-    protected $guarded = [];
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'converted_for_downloading_at',
+        'converted_for_streaming_at',
+        'created_at',
+        'updated_at',
+    ];
+
+    // Add any relationships, additional methods, or other configurations as needed
 }
